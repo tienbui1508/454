bc5a20bb6319b7854c7cca9d6a9abcdd75c65b78
Tien Bui <tienbui1508@gmail.com>
2024-02-26 02:41:13 +1030

    Add project files.

---
+++ 454Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _454Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
