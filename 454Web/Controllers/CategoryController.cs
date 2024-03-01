fa36f3b4695d9e124f48d01ae97d122a58851ab0
Tien Bui <tienbui1508@gmail.com>
2024-03-02 03:32:21 +1030

    Category controller, view draft
    first draft. commit to move working space to pc

---
+++ 454Web/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using _454Web.Data;
+using _454Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _454Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+    }
+}
